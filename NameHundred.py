4d89e11b9b43a975fc8be39e6678503e7bb8cc16
Birgit <birgit.schoenfeld@iubh-fernstudium.de>
2020-06-02 12:41:32 +0200

    Update NameHundred.py
    add task description

--- NameHundred.py
+++ NameHundred.py
@@ -1,3 +1,9 @@
+#Task 1
+#Ask User for their name and age.
+#Print message adressed to user, telling themm how many years until they turn 100.
+
+
+
 name = input ("What is your name? ")
 print ("Hello " + name)
 age = input ("How old are you? ")
