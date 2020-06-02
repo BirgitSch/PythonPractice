7a50af9c9c19a4d222b2fff5e3a38bdc4f54976a
Birgit <timundvin@web.de>
2020-06-02 12:37:36 +0200

    Add files via upload

---
+++ NameHundred.py
@@ -0,0 +1,7 @@
+name = input ("What is your name? ")
+print ("Hello " + name)
+age = input ("How old are you? ")
+age = int(age)
+leftyears = 100 - age
+leftyears = str(leftyears)
+print (name + " you have " + leftyears + " years left until you turn 100!")
